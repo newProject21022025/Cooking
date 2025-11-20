0bc85bf823a9eb8f7d722e102c29f9ab662ec1c1
dap-13 <andy73594@gmail.com>
2025-11-20 12:04:15 +0200

    work with articals

--- frontend/src/app/[locale]/articles/[id]/page.tsx
+++ frontend/src/app/[locale]/articles/[id]/page.tsx
@@ -1,48 +1,48 @@
-// src/app/[locale]/articles/[id]/page.tsx
+// // src/app/[locale]/articles/[id]/page.tsx
 
-import { fetchArticleById } from "@/api/articleApi";
-import { Article, ArticleBlock } from "@/types/article";
+// import { fetchArticleById } from "@/api/articleApi";
+// import { Article, ArticleBlock } from "@/types/article";
 
-async function getArticle(id: number) {
-  const article = await fetchArticleById(id);
-  return article;
-}
+// async function getArticle(id: number) {
+//   const article = await fetchArticleById(id);
+//   return article;
+// }
 
-const currentLocale = "ua";
+// const currentLocale = "ua";
 
-export default async function ArticlePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const article = await getArticle(Number(params.id));
+// export default async function ArticlePage({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
+//   const article = await getArticle(Number(params.id));
 
-  const title = article.title[currentLocale as keyof Article["title"]];
-  const introDescription =
-    article.description[currentLocale as keyof Article["description"]];
+//   const title = article.title[currentLocale as keyof Article["title"]];
+//   const introDescription =
+//     article.description[currentLocale as keyof Article["description"]];
 
-  return (
-    <article>
-      <h1>{title}</h1>
+//   return (
+//     <article>
+//       <h1>{title}</h1>
 
-      <img src={article.photo} alt={title} className="article-main-photo" />
+//       <img src={article.photo} alt={title} className="article-main-photo" />
 
-      <p className="article-intro">{introDescription}</p>
+//       <p className="article-intro">{introDescription}</p>
 
-      <section className="article-content">
-        {article.blocks.map((block, index) => (
-          <div key={index} className="content-block">
-            <h2>{block.title[currentLocale as keyof ArticleBlock["title"]]}</h2>
-            <p>
-              {
-                block.description[
-                  currentLocale as keyof ArticleBlock["description"]
-                ]
-              }
-            </p>
-          </div>
-        ))}
-      </section>
-    </article>
-  );
-}
+//       <section className="article-content">
+//         {article.blocks.map((block, index) => (
+//           <div key={index} className="content-block">
+//             <h2>{block.title[currentLocale as keyof ArticleBlock["title"]]}</h2>
+//             <p>
+//               {
+//                 block.description[
+//                   currentLocale as keyof ArticleBlock["description"]
+//                 ]
+//               }
+//             </p>
+//           </div>
+//         ))}
+//       </section>
+//     </article>
+//   );
+// }
